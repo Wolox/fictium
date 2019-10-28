b809f62a0abb34d61beaf32d83cdfd5b246ada88
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2019-10-28 11:36:25 -0300

    now headers are extracted from params

--- lib/fictium/rspec/autocomplete/params.rb
+++ lib/fictium/rspec/autocomplete/params.rb
@@ -72,6 +72,62 @@ module Fictium
           def ignored_header_groups
             Fictium.configuration.ignored_header_groups
           end
+
+          private
+
+          def parse_request_query(params, _action, request)
+            parse_query_values(request.query_string).each do |key, value|
+              params[key] ||= {}
+              params[key].merge!(
+                example: value
+              )
+            end
+          end
+
+          def parse_request_header(params, _action, request)
+            request.headers.to_h.each do |name, value|
+              next unless valid_header?(name)
+
+              params[name] ||= {}
+              params[name].merge!(
+                example: value
+              )
+            end
+          end
+
+          def parse_request_path(params, action, _request)
+            action.full_path.scan(PATH_TEMPLATE).flatten.each do |name|
+              params[name] ||= {}
+              # TODO: Extract example from request
+            end
+          end
+
+          def parse_request_cookie(params, _action, request)
+            request.cookies.each do |key, value|
+              params[key] ||= {}
+              params[key].merge!(
+                example: value
+              )
+            end
+          end
+
+          def valid_header?(name)
+            return false if ignored_header_groups.any? { |group| name.downcase.start_with?(group) }
+
+            !Fictium.configuration.ignored_header_values.include?(name.downcase)
+          end
+
+          def ignored_header_groups
+            Fictium.configuration.ignored_header_groups
+          end
+
+          def parse_query_values(query_string)
+            if defined?(Rack::Utils)
+              Rack::Utils.parse_nested_query(query_string.presence || '')
+            else
+              CGI.parse(query_string.presence || '')
+            end
+          end
         end
       end
     end
