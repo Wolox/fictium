33e4e5bfd288e33448b42bff1c15ab3583239c3e
Samir Tapiero <samir.hernando@wolox.co>
2019-12-26 21:59:16 -0500

    Add formatter to body

--- lib/fictium/exporters/open_api/v3_exporter/content_formatter.rb
+++ lib/fictium/exporters/open_api/v3_exporter/content_formatter.rb
@@ -2,17 +2,25 @@ module Fictium
   module OpenApi
     class V3Exporter
       class ContentFormatter
+        APP_JSON = 'application/json'.freeze
+
         def format(http_object, default = nil)
           type = (http_object.presence && http_object[:content_type].presence) || default
           return if type.blank?
 
           {}.tap do |content|
             media_type = {
-              example: http_object[:body]
+              example: body_formatter(http_object[:content_type], http_object[:body])
             }
             media_type[:schema] = http_object[:schema] if http_object[:schema].present?
             content[type.to_sym] = media_type
           end
+        end
+
+        protected
+
+        def body_formatter(content_type, body)
+          content_type == APP_JSON ? JSON.parse(body) : body
         end
       end
     end
